adaf01fa04e9f78d249c92b010120f538cede110
Franklin P. Ramos <franky88@users.noreply.github.com>
2023-12-01 11:23:22 +0800

    adding bash command to restart nginx service

---
+++ reload_service.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "Today is " `date`
+
+sudo systemctl restart gunicorn
+sudo systemctl daemon-reload
+sudo systemctl restart gunicorn.socket gunicorn.service
+sudo nginx -t && sudo systemctl restart nginx
