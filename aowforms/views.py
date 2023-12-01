4cb109dea40fef1805e17c0aff3778fcd139fbc9
Franklin P. Ramos <franky88@users.noreply.github.com>
2023-12-01 11:13:27 +0800

    updates

--- aowforms/views.py
+++ aowforms/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render, HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Page is live!")
+    return HttpResponse("Page is live updated!")
